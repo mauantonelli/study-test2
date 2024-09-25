1056b6bab75acd5c3e9b1134e3c66b5bf001fbd6
mauantonelli <pablojuanang@gmail.com>
2024-09-25 17:52:13 -0300

    localdate test

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,22 +28,30 @@ public class Cliente extends Pessoa {
     }
 
     public void exibirHistoricoCompras() {
+        LocalDate dataLimite = LocalDate.of(2023, 9, 1);
+
         for (Compra compra : compras) {
             System.out.println("Compra nº: " + compra.getNumeroCompra() +
-                    " | Data: " + compra.getDataCompra() +
                     " | Cliente: " + compra.getCliente().getNome() +
                     " | Vendedor: " + compra.getVendedor().getNome() +
                     " | Total a Pagar: R$" + compra.calcularValorTotalCompra());
 
+            if (compra.getDataCompra().isAfter(dataLimite)) {
+                System.out.println("Exibindo compra após a data limite" + compra.getDataCompra());
+            } else {
+                System.out.println("Data não permitida!");
+            }
 
             System.out.println("Itens comprados:");
             for (ItemCompra item : compra.getItens()) {
                 System.out.println("Produto: " + item.getProduto().getDescricao() +
                         " | Quantidade: " + item.getQuantidadeComprada() +
-                        " | Valor Total: R$" + item.getValorTotal());
+                        " | Valor Total: R$" + item.calcularValorTotal());
             }
+
             System.out.println("-----------------------------------");
         }
     }
+
 
 }
